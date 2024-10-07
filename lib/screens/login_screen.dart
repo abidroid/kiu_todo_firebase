e4f0b93356d91197f5d5bce148724c2f702463de
Abid <abidroid1@gmail.com>
2024-10-07 12:40:16 +0500

    screens created

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:kiu_todo_firebase/screens/forgot_password_screen.dart';
+import 'package:kiu_todo_firebase/screens/sign_up_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: const Text('Login Screen'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Email',
+              ),
+            ),
+            const Gap(16),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Password',
+              ),
+            ),
+            Align(
+                alignment: Alignment.topRight,
+                child: TextButton(onPressed: (){
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context){
+                    return const ForgotPasswordScreen();
+                  }));
+                }, child: const Text('Forgot Password?'))),
+
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  foregroundColor: Colors.white,
+                ),
+                onPressed: (){}, child: const Text('LOGIN')),
+
+            const Gap(16),
+
+            TextButton(onPressed: (){
+              Navigator.of(context).push(MaterialPageRoute(builder: (context){
+                return const SignUpScreen();
+              }));
+
+            }, child: const Text('Not Registered Yet? SignUp Now'))
+
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/update_task_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UpdateTaskScreen extends StatefulWidget {
+  const UpdateTaskScreen({super.key});
+
+  @override
+  State<UpdateTaskScreen> createState() => _UpdateTaskScreenState();
+}
+
+class _UpdateTaskScreenState extends State<UpdateTaskScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
