4f1872f8dbdf18a7c49e2278294275278168fff9
Abid <abidroid1@gmail.com>
2024-10-10 12:57:02 +0500

    forgot password done

--- lib/screens/forgot_password_screen.dart
+++ lib/screens/forgot_password_screen.dart
@@ -1,4 +1,4 @@
-
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ForgotPasswordScreen extends StatefulWidget {
@@ -9,8 +9,31 @@ class ForgotPasswordScreen extends StatefulWidget {
 }
 
 class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
+
+  var emailC = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Forogt Password ?'),
+      ),
+      body: ListView(
+        children: [
+          TextField(controller: emailC,),
+          ElevatedButton(
+              onPressed: () async {
+
+                String email = emailC.text.trim();
+
+                await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
+
+                Navigator.of(context).pop();
+
+
+              }, child: const Text('Send Password Reset Email'))
+        ],
+      ),
+    );
   }
 }
